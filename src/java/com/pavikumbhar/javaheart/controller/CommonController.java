6e7473a85e5e4a0ae103d390ca00f12f9d2ca179
=pavi <=pavikumbhar007@gmail.com>
2017-04-12 23:33:09 +0530

    1

---
+++ src/java/com/pavikumbhar/javaheart/controller/CommonController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pavikumbhar.javaheart.controller;
+
+import com.pavikumbhar.javaheart.common.controller.MasterController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author pavikumbhar
+ */
+public class CommonController {
+    
+}
+
+
+ 
+@RestController
+@RequestMapping("/user/")
+ class User extends MasterController<User, Long>{
+    
+}
